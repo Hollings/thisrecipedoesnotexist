5b5d617f456d3d0bf2dc70c2f9a4cac6fc546907
John Hollingsworth <hollingsfsdf@gmail.com>
2019-05-18 18:25:39 -0600

    Added google analytics

--- site/config/app.php
+++ site/config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        Ipunkt\LaravelAnalytics\AnalyticsServiceProvider::class,
+
+
+
     ],
 
     /*
@@ -225,6 +230,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+
+        'Analytics' => Ipunkt\LaravelAnalytics\AnalyticsFacade::class,
+
+        
 
     ],
 
