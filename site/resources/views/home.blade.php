26ed90b7ac9855efea15c4cd4c422f018769aede
John Hollingsworth <hollingsfsdf@gmail.com>
2019-05-25 14:23:48 -0600

    Set a max width on search

--- site/resources/views/home.blade.php
+++ site/resources/views/home.blade.php
@@ -21,6 +21,12 @@
     .recent-recipes {
     font-size: 12px;
     }
+    .typeahead.dropdown-menu {
+        max-width: 300px;
+    }
+    .typeahead .dropdown-item{
+        white-space: initial;
+    }
     </style>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1">
