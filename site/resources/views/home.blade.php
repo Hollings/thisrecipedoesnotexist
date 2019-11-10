2d1fb676aaf87ff1ffeb8ae9cf2524988547565f
John Hollingsworth <hollingsfsdf@gmail.com>
2019-11-09 18:28:21 -0700

    testing some meta content

--- site/resources/views/home.blade.php
+++ site/resources/views/home.blade.php
@@ -58,6 +58,13 @@
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js"></script>  
+
+    <meta content="This Recipe Does Not Exist" property="og:title">
+
+    <meta content="{{ $r->title }}: {{ count(json_decode($r->ingredients)) }} Ingredients" property="og:description">
+
+    <meta content="thisrecipedoesnotexist.com" property="og:site_name">
+
     {!! Analytics::render() !!}
 <title>{{ $r->title }}</title>
 </head>
